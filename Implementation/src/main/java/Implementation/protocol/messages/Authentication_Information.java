7ec28243685eae0e65d78b1cacfa64553ca87d90
raphaelbroesamle <raphaelbroesamle@mail>
2019-10-19 14:32:26 +0200

    Added some TODOs in the code for the 'Differences' section in the paper.

--- Implementation/src/main/java/Implementation/protocol/messages/Authentication_Information.java
+++ Implementation/src/main/java/Implementation/protocol/messages/Authentication_Information.java
@@ -2,6 +2,7 @@ package Implementation.protocol.messages;
 
 import Implementation.structure.Message;
 
+//TODO
 //This message is for informing the UDM about the (un)successful authentication.
 public class Authentication_Information implements Message {
 
--- Implementation/src/main/java/Implementation/protocol/messages/Nausf_UEAuthentication_Confirmation_Request.java
+++ Implementation/src/main/java/Implementation/protocol/messages/Nausf_UEAuthentication_Confirmation_Request.java
@@ -2,6 +2,7 @@ package Implementation.protocol.messages;
 
 import Implementation.structure.Message;
 
+//TODO
 //In the documentation this message is called 'Nausf_UEAuthentication Authenticate Request'.
 //It was renamed because an earlier message has the same name.
 public class Nausf_UEAuthentication_Confirmation_Request implements Message {
